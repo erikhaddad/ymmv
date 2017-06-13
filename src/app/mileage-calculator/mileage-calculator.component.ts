dfed8f18c8ed04e893929e8a3f3a1be3d30200de
edh <edh@google.com>
2017-06-13 03:16:29 -0700

    mileage-calculator complete

--- src/app/mileage-calculator/mileage-calculator.component.ts
+++ src/app/mileage-calculator/mileage-calculator.component.ts
@@ -1,5 +1,5 @@
 import {Component, OnInit, ViewEncapsulation, ChangeDetectionStrategy} from '@angular/core';
-import {FormControl} from '@angular/forms';
+import {AbstractControl, FormBuilder, FormControl, FormGroup, Validators} from '@angular/forms';
 
 import {IAirport} from '../common/data-model';
 
@@ -19,21 +19,8 @@ import * as _ from 'lodash';
 })
 export class MileageCalculatorComponent implements OnInit {
 
-    selectedOriginAirportCode: string;
-    selectedOriginAirportArr: string[];
-    selectedOriginAirportObj: IAirport;
-    selectedOriginDateObj: Date;
-    selectedOriginHelperText: string;
-
-    selectedDestinationAirportCode: string;
-    selectedDestinationAirportArr: string[];
-    selectedDestinationAirportObj: IAirport;
-    selectedDestinationDateObj: Date;
-    selectedDestinationHelperText: string;
-
-    selectedAirlineCode: string;
-    selectedAirlineObj: IAirport;
-    selectedAirlineHelperText: string;
+    originHintText: string;
+    destinationHintText: string;
 
     calculatedMiles: number;
 
@@ -45,35 +32,29 @@ export class MileageCalculatorComponent implements OnInit {
         polylines: [] // array of lines to display
     };
 
+    airportsForm: FormGroup;
+
     airportCtrl: FormControl;
     filteredAirports: any;
 
     airports$: FirebaseListObservable<IAirport[]>;
     airports: IAirport[];
 
-    constructor(public dataService: DataService) {
-        this.selectedOriginAirportCode = '';
-        this.selectedOriginAirportArr = [null, null, null];
-        this.selectedOriginAirportObj = null;
-        this.selectedOriginDateObj = new Date();
-        this.selectedOriginHelperText = '3 letter airport code';
+    constructor(public dataService: DataService, private fb: FormBuilder) {
 
-        this.selectedDestinationAirportCode = '';
-        this.selectedDestinationAirportArr = [null, null, null];
-        this.selectedDestinationAirportObj = null;
-        this.selectedDestinationDateObj = new Date();
-        this.selectedDestinationHelperText = '3 letter airport code';
+        this.originHintText = '(e.g. SFO)';
+        this.destinationHintText = '(e.g. JFK)';
 
-        this.selectedAirlineCode = '';
-        this.selectedAirlineObj = null;
-        this.selectedAirlineHelperText = 'Airline and flight number';
-
+        this.airports = [];
         this.airports$ = dataService.airports;
         this.airports$.subscribe(airports => {
             this.airports = _.orderBy(airports, 'name', 'asc');
+
         });
 
         this.calculatedMiles = 0;
+
+        this.createForm();
 
         this.airportCtrl = new FormControl();
         this.filteredAirports = this.airportCtrl.valueChanges
@@ -81,12 +62,148 @@ export class MileageCalculatorComponent implements OnInit {
             .map(name => this.filterAirports(name));
     }
 
+    airportMatcher(control: FormControl, key: string): {[key: string]: boolean} {
+        const airportCode = control.value;
+
+        if (airportCode.length === 0) {
+            if (key === 'origin') {
+                this.originHintText = '(e.g. SFO)';
+            } else if (key === 'destination') {
+                this.destinationHintText = '(e.g. JFK)';
+            }
+
+            return null;
+        }
+
+        if (airportCode.length < 3) {
+            if (key === 'origin') {
+                this.originHintText = 'Not long enough';
+            } else if (key === 'destination') {
+                this.destinationHintText = 'Not long enough';
+            }
+
+            return null;
+        }
+
+        const airport = this.getAirportByCode(airportCode);
+
+        if (airport) {
+            if (key === 'origin') {
+                this.originHintText = airport.name;
+            } else if (key === 'destination') {
+                this.destinationHintText = airport.name;
+            }
+
+            return null;
+        } else {
+            if (key === 'origin') {
+                this.originHintText = 'Airport not found';
+            } else if (key === 'destination') {
+                this.destinationHintText = 'Airport not found';
+            }
+
+            return { nomatch: true };
+        }
+    }
+
+    createForm() {
+        this.airportsForm = this.fb.group({
+            origin: ['',
+                [
+                    Validators.required,
+                    Validators.minLength(3),
+                    (formControl: FormControl) => {
+                        return this.airportMatcher(formControl, 'origin');
+                    }
+                ]
+            ],
+            destination: ['',
+                [
+                    Validators.required,
+                    Validators.minLength(3),
+                    (formControl: FormControl) => {
+                        return this.airportMatcher(formControl, 'destination');
+                    }
+                ]
+            ],
+        });
+    }
+
+    onSubmit(evt: Event) {
+        const originAirportCode = this.airportsForm.get('origin').value,
+            destinationAirportCode = this.airportsForm.get('destination').value;
+
+        const originObj = this.getAirportByCode(originAirportCode),
+            destinationObj = this.getAirportByCode(destinationAirportCode);
+
+        if (originObj && destinationObj) {
+            const originLatitude = Number.parseFloat(originObj.latitude),
+                originLongitude = Number.parseFloat(originObj.longitude),
+                destinationLatitude = Number.parseFloat(destinationObj.latitude),
+                destinationLongitude = Number.parseFloat(destinationObj.longitude);
+
+            this.calculatedMiles = this.calculateSegmentMiles(originObj, destinationObj);
+
+            this.mapConfig.markers = [
+                {
+                    id: 'origin-' + originAirportCode,
+                    position: {
+                        lat: originLatitude,
+                        lng: originLongitude
+                    },
+                    showWindow: false,
+                    title: originObj.city + ', ' + originObj.country,
+                    animation: 0
+                },
+                {
+                    id: 'destination-' + destinationAirportCode,
+                    position: {
+                        lat: destinationLatitude,
+                        lng: destinationLongitude
+                    },
+                    showWindow: false,
+                    title: destinationObj.city + ', ' + destinationObj.country,
+                    animation: 0
+                }
+            ];
+
+            this.mapConfig.polylines = [
+                {
+                    id: 'calculated-path',
+                    paths: [
+                        {
+                            lat: originLatitude,
+                            lng: originLongitude
+                        },
+                        {
+                            lat: destinationLatitude,
+                            lng: destinationLongitude
+                        }
+                    ],
+                    stroke: {
+                        // color: "#039be5",
+                        color: '#ffab40',
+                        opacity: 0.5,
+                        weight: 2
+                    },
+                    editable: false,
+                    geodesic: true,
+                    visible: true
+                }
+            ];
+        }
+    }
+
+    onClick(evt: Event) {
+        console.log('click');
+    }
+
     ngOnInit() {
 
     }
 
     getAirportByCode(code: string): IAirport {
-        return _.find(this.airports, {'code': code});
+        return _.find(this.airports, {'code': code ? code.toUpperCase() : ''});
     }
 
     filterAirports(val: string) {
@@ -112,10 +229,7 @@ export class MileageCalculatorComponent implements OnInit {
         return Math.round(Math.acos(Math.sin(latOne) * Math.sin(latTwo) + Math.cos(latOne) * Math.cos(latTwo) * Math.cos(lonChange)) * R);
     }
 
-    private calculateSegmentMiles(origin, destination) {
-        const originObj = this.getAirportByCode(origin),
-            destinationObj = this.getAirportByCode(destination);
-
+    private calculateSegmentMiles(originObj, destinationObj) {
         let estimation = null;
         if (!!originObj && !!destinationObj) {
             estimation = this.calculateDistance(originObj.latitude, destinationObj.latitude,
